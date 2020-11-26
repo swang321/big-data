418f5d9aaf1b60a97f4ac381a6eb344b798179c0
978530201@qq.com <gjm456>
2020-11-26 15:37:15 +0800

    补充

--- boot-hbase/src/main/java/com/hadoop/hbase/HbaseApplication.java
+++ boot-hbase/src/main/java/com/hadoop/hbase/HbaseApplication.java
@@ -1,13 +1,13 @@
-package com.big.data.demo;
+package com.hadoop.hbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class HbaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(HbaseApplication.class, args);
     }
 
 }
